3d6f83403a92b46d58535c64c8219a1aeb05ddfd
Aniket Nandan <nandan.aniket3@gmail.com>
2015-09-03 11:19:21 +0530

    modified

--- assgn3.js
+++ assgn3.js
@@ -5,23 +5,30 @@ function loadrepo(url){
     if(window.XMLHttpRequest){
       xhr=new XMLHttpRequest();
     }
-    xhr.onreadystatechange=function(){
+    xhr.onload=function(){
       if(xhr.readyState==4 && xhr.status==200){
         console.log("1");
-        t(JSON.parse(xhr.responseText).items[0]);
+        t(console.log(JSON.parse(xhr.responseText).items[0]));
       }
     }
     xhr.onerror=function(){
       console.log("2");
-      f("Error in finding!");
+      var er=new Error("There is an error!");
+      f(er);
     }
+    xhr.open("GET", url, true);
+    xhr.send();
   })
-
-  xhr.open("GET", url, true);
-  xhr.send();
 }
-
 
 var val = document.querySelector("#repo_name").value;
 loadrepo("https://api.github.com/search/repositories?q="+val)
-  .then(function(res){console.log("23")}, function(res){console.log(res)});
+  .then(function(res){
+    console.log("23");
+  },
+        function(res){
+          alert(res);
+        })
+  .catch(function(){
+    alert("Promise is rejected!");
+  })
